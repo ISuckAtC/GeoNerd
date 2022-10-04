f5b2b9f8b107c0025a5cd72c7ab8569d8bcd47c1
Henrik <theporkchopsxd@gmail.com>
2022-10-04 12:37:53 +0200

    Riuhsfhdj

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -20,7 +20,7 @@ public class LoadScene : MonoBehaviour
 
     void Start()
     {
-        if (GameManager.Flags[Flag.OSLO_FORESTDONE] && GameManager.Flags[Flag.OSLO_OPERAPUZZLECOMPLETE] && GameManager.Flags[Flag.OSLO_LIBRARY_DONE])
+        if (GameManager.Flags[Flag.OSLO_FORESTDONE] && GameManager.Flags[Flag.OSLO_OPERAPUZZLECOMPLETE] && GameManager.Flags[Flag.OSLO_LIBRARYDONE])
         {
             realCase.SetActive(false);
             fakeCase.SetActive(true);
