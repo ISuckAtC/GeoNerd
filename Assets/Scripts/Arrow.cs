b4466b7521680ed1e939a9613f34548ceb736daf
ISuckAtC <theporkchopsxd@gmail.com>
2022-10-07 11:05:42 +0200

    arrow

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private GameObject objToFace;
+
+    private bool isArrow = false;
+
+    public GameObject office, oslo, forest, trondheim;
+
+    void Start()
+    {
+        if (GameManager.Flags[Flag.OFFICE_ARROW])
+        {
+            objToFace = office;
+            isArrow = true;
+        }
+        if (GameManager.Flags[Flag.OSLO_ARROW])
+        {
+            objToFace = oslo;
+            isArrow = true;
+        }
+        if (GameManager.Flags[Flag.FOREST_ARROW])
+        {
+            objToFace = forest;
+            isArrow = true;
+        }
+        if (GameManager.Flags[Flag.TRONDHEIM_ARROW])
+        {
+            objToFace = trondheim;
+            isArrow = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (isArrow)
+        {
+            transform.LookAt(objToFace.transform.position);
+        }
+    }
+}
--- Assets/Scripts/FaceGameObject.cs
+++ Assets/Scripts/FaceGameObject.cs
@@ -4,41 +4,12 @@ using UnityEngine;
 
 public class FaceGameObject : MonoBehaviour
 {
-    private GameObject objToFace;
-
-    private bool isArrow = false;
-
-    public GameObject office, oslo, forest, trondheim;
+    public GameObject objToFace;
     
     
-    void Start()
-    {
-        if (GameManager.Flags[Flag.OFFICE_ARROW])
-        {
-            objToFace = office;
-            isArrow = true;
-        }
-        if (GameManager.Flags[Flag.OSLO_ARROW])
-        {
-            objToFace = oslo;
-            isArrow = true;
-        }
-        if (GameManager.Flags[Flag.FOREST_ARROW])
-        {
-            objToFace = forest;
-            isArrow = true;
-        }
-        if (GameManager.Flags[Flag.TRONDHEIM_ARROW])
-        {
-            objToFace = trondheim;
-            isArrow = true;
-        }
-    }
+    
     void FixedUpdate()
     {
-        if (isArrow)
-        {
-            transform.LookAt(objToFace.transform.position);
-        }
+        transform.LookAt(objToFace.transform.position);
     }
 }
