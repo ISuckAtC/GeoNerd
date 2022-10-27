f7eab2964333245ff1c36f7e12d7c0285400b8de
Henrik <theporkchopsxd@gmail.com>
2022-10-27 13:03:11 +0200

    fmod more stuff

--- Assets/Scripts/PlayOnStart.cs
+++ Assets/Scripts/PlayOnStart.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayOnStart : MonoBehaviour
 {
     public FMODUnity.EventReference reference;
     public Vector3 velocity;
     public float volume;
+    public int persist;
+    private FMOD.Studio.EventInstance instance;
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.FMODPlayStatic(reference, transform.position, velocity, volume);
+        instance = GameManager.FMODPlayStatic(reference, transform.position, velocity, volume, persist == 0);
+        if (persist > 0)
+        {
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += Persist;
+        }
+    }
+
+    void Persist(Scene scene, LoadSceneMode mode)
+    {
+        if (--persist == 0)
+        {
+            instance.release();
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
