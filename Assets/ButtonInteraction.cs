e4a4de449049bc6c3004c5948286f78cfba9f50b
madelino22 <madelino@ucm.es>
2022-10-27 12:11:12 +0200

    Final main menu functionality

--- Assets/ButtonInteraction.cs
+++ Assets/ButtonInteraction.cs
@@ -7,11 +7,22 @@ using DG.Tweening;
 
 public class ButtonInteraction : MonoBehaviour
 {
+    static ButtonInteraction selectedOne = null;
     // Start is called before the first frame update
     float initialScale;
+    float initialPos;
+    public int movement;
+    public bool initialSelected = false;
+
     void Start()
     {
         initialScale = transform.localScale.x;
+        initialPos = transform.position.x;
+        if (initialSelected)
+        {
+            selectedOne = this;
+            transform.position = new Vector3(initialPos + movement, transform.position.y, transform.position.z); ;
+        }
     }
     
 
@@ -21,14 +32,35 @@ public class ButtonInteraction : MonoBehaviour
         
     }
 
+   
 
-    public void OnMouseEnter(BaseEventData data)
+    public void NewOneSelected()
+    {
+        if (selectedOne != this)
+        {
+            if(selectedOne)
+            selectedOne.transform.DOMoveX(initialPos, 0.5f);
+        }
+        selectedOne = this;
+    }
+    public void OnMouseEnterSize(BaseEventData data)
     {
         transform.DOScale(initialScale*1.25f, 0.5f);
     }
-    public void onMouseLeaves(BaseEventData data)
+    public void OnMouseLeavesSize(BaseEventData data)
     {
         transform.DOScale(initialScale * 1f, 0.5f);
+    }
+
+    public void OnMouseEnterHorizontalSlide(BaseEventData data)
+    {
+        if (selectedOne != this)
+        transform.DOMoveX(initialPos + movement, 0.5f);
+    }
+    public void OnMouseLeavesHorizontalSlide(BaseEventData data)
+    {
+        if (selectedOne != this)
+            transform.DOMoveX(initialPos, 0.5f);
     }
 
 }
