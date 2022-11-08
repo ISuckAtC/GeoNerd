24be1f34c5b06529864f8c722dadfca967c44a96
Rune Nohr <77842104+JeyesElite@users.noreply.github.com>
2022-11-08 12:21:00 +0100

    asdfæoijhn

---
+++ Assets/Scripts/RaycastOffice.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastOffice : MonoBehaviour
+{
+    public Transform camera;
+    private CameraFollow cf;
+
+    public GameObject file;
+    
+    
+    void Start()
+    {
+        cf = camera.gameObject.GetComponent<CameraFollow>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 9999f, 1<<9))
+            {
+                Debug.Log(hit.collider.name);
+            
+                if (hit.collider.CompareTag("Office"))
+                {
+                    if(!file.activeSelf)
+                        cf.ToggleOffice();
+                }
+                else if (hit.collider.CompareTag("CaseFile"))
+                {
+                    file.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
