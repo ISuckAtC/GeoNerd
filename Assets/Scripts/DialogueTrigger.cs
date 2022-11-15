cf2efbbd7ceb3e62b39f56dfd07e579aa7a6c0c9
madelino22 <madelino@ucm.es>
2022-11-15 12:45:29 +0100

    Outlines in the UI

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
@@ -10,7 +11,11 @@ public class DialogueTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExit
     [SerializeField] DialogueType type = DialogueType.Default;
     [SerializeField] GameObject enableObject = null; 
     public Dialogue dialogue;
-
+    private Outline outline;
+    private void Start()
+    {
+        outline = GetComponent<Outline>();
+    }
     public void TriggerDialogue()
     {
         DialogueManager.GetInstance().StartDialogue(dialogue, type);
@@ -23,12 +28,15 @@ public class DialogueTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("Mouse enter");
+        if (outline) outline.enabled = true;
         Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         //Debug.Log("Mouse exit");
+        if (outline) outline.enabled = false;
+
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 }
